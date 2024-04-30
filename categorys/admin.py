746bb90148d5ad31fba965e9269e0e4508621e6b
Álisson <alissonwegner@gmail.com>
2024-04-30 00:19:32 -0300

    teste iniciais com categorys

--- categorys/admin.py
+++ categorys/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from categorys.models import Categorys
 
+@admin.register(Categorys)
+class CategorysAdmin(admin.ModelAdmin):
+    list_display = ('category_id', 'categoria_txt')
 # Register your models here.
--- categorys/views.py
+++ categorys/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from categorys.models import Categorys
 
-# Create your views here.
+def category_view(request):
+    categorys = Categorys.objects.all()
+    data = [{'id': category.category_id, 'texto': category.categoria_txt} for category in categorys]
+
+    return JsonResponse(data, safe=False)
